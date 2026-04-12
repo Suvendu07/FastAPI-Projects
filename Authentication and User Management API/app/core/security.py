30aa228b84571902456167ab7155415f87c409be
Sp09 <khuntiasuvendu843@gmail.com>
2026-04-12 23:35:51 +0530

    initialize commit

--- Authentication and User Management API/app/core/security.py
+++ Authentication and User Management API/app/core/security.py
@@ -14,7 +14,7 @@ def verify_password(password: str, hashed_password: str) ->bool:
     return pwd_context.verify(password, hashed_password)
 
 
-def create_access_token(data: dict, expires_minutes: int = 30):
+def create_access_token(data: dict, expires_minutes: int = 60):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
     to_encode.update({"exp": expire})
